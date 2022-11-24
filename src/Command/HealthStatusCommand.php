ea529f78e3540e4fa8f01f11cffc104b98757e3d
ketut-angga <ketut.angga@balabali.com>
2022-11-24 10:57:54 +0800

    add server data information

--- src/Command/HealthStatusCommand.php
+++ src/Command/HealthStatusCommand.php
@@ -4,10 +4,13 @@ namespace Solvrtech\Laravel\Logbook\Command;
 
 use Exception;
 use Illuminate\Console\Command;
+use Solvrtech\Laravel\Logbook\LogbookConfig;
 use Symfony\Component\HttpClient\HttpClient;
 
 class HealthStatusCommand extends Command
 {
+    use LogbookConfig;
+
     /**
      * The name and signature of the console command.
      *
@@ -38,7 +41,8 @@ class HealthStatusCommand extends Command
                     'headers' => [
                         'Content-Type' => 'application/json',
                         'Accept' => 'application/json',
-                        'token' => $this->getAPIkey(),
+                        'x-lb-token' => $this->getAPIkey(),
+                        'x-lb-version' => $this->getVersion()
                     ],
                     'body' => "",
                 ]
@@ -47,45 +51,5 @@ class HealthStatusCommand extends Command
         }
 
         return Command::SUCCESS;
-    }
-
-    /**
-     * Get logbook API url from environment.
-     *
-     * @return string
-     *
-     * @throws Exception
-     */
-    private function getAPIUrl(): string
-    {
-        $url = config('logbook.api.url');
-
-        if (null === $url) {
-            throw new Exception('Logbook url not found');
-        }
-
-        if ('/' === substr($url, -1)) {
-            $url = substr($url, 0, -1);
-        }
-
-        return $url;
-    }
-
-    /**
-     * Get logbook API key from environment.
-     *
-     * @return string
-     *
-     * @throws Exception
-     */
-    private function getAPIKey(): string
-    {
-        $key = config('logbook.api.key');
-
-        if (null === $key) {
-            throw new Exception('Logbook key not found');
-        }
-
-        return $key;
     }
 }
